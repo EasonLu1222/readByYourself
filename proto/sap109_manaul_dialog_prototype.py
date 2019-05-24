a0af962b5a9d7818b0bc91b01302b8668ab35919
zealzel <zealzel@gmail.com>
2019-05-24 13:23:36 +0800

    modify sap109_manaul_dialog_prototype.py to implement hotkey

--- proto/sap109_manaul_dialog_prototype.py
+++ proto/sap109_manaul_dialog_prototype.py
@@ -1,5 +1,5 @@
 import json
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 
 
 class Page(QtWidgets.QWidget):
@@ -11,73 +11,41 @@ class Page(QtWidgets.QWidget):
         self.setLayout(layout)
 
 
-class MyWizard(QtWidgets.QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        print("DFSDFSD")
-
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.CustomizeWindowHint)
-        self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
-        self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, False)
-        self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, False)
-
-        self.number = 4
-        self.pages = []
-        self.idx = 0
-        self.passes = {}
-
-        vertical_layout = QtWidgets.QVBoxLayout()
-        self.setLayout(vertical_layout)
-
-        self.content_layout = QtWidgets.QVBoxLayout() # could be almost any layout actually
-        vertical_layout.addLayout(self.content_layout)
-
-        for i in range(self.number):
-            content = Page(i)
-            self.pages.append(content)
-        self.content_layout.addWidget(self.pages[0])
-
-        button_layout = QtWidgets.QHBoxLayout()
-        button_layout.addStretch()
-
-        fail_button = QtWidgets.QPushButton('Failed')
-        fail_button.setObjectName('fail_button')
-        fail_button.clicked.connect(self.button_clicked)
-        button_layout.addWidget(fail_button)
-
-        pass_button = QtWidgets.QPushButton('Passed')
-        pass_button.setObjectName('pass_button')
-        pass_button.clicked.connect(self.button_clicked)
-        button_layout.addWidget(pass_button)
-
-        vertical_layout.addLayout(button_layout)
-        self.show()
-
-    def button_clicked(self):
-        btn = self.sender()
-        if btn.objectName() == 'pass_button':
-            self.passes[self.idx] = True
-        elif btn.objectName() == 'fail_button':
-            self.passes[self.idx] = False
-
-        if self.idx < self.number-1:
-            content = self.pages[self.idx]
-            self.content_layout.removeWidget(content)
-            content.deleteLater()
-            self.content_layout.addWidget(self.pages[self.idx+1])
-        else:
-            print('final_result')
-            print(self.passes)
-            self.close()
-        self.idx += 1
-
-
 
 class MyDialog(QtWidgets.QDialog):
     message = QtCore.pyqtSignal(str)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        #  self.setStyleSheet("""
+            #  QPushButton:focus {
+                    #  border: none;
+                    #  outline: none;
+            #  }
+        #  """)
+
+        self.setStyleSheet("""
+            QPushButton { 
+                padding: 6px; 
+                background: #369; 
+                color: white; 
+                font-size: 33px;
+                border: 0; 
+                border-radius: 3px; 
+                outline: 0px; 
+            }
+            QPushButton:hover { 
+                background: #47a; 
+            }
+            QPushButton:pressed { 
+                background: #58b; 
+            }
+        """)
+
+
+
+        self.setModal(True)
+
         self.setWindowFlags(self.windowFlags() | QtCore.Qt.CustomizeWindowHint)
         self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
         self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, False)
@@ -102,19 +70,31 @@ class MyDialog(QtWidgets.QDialog):
         button_layout = QtWidgets.QHBoxLayout()
         button_layout.addStretch()
 
+        pass_button = QtWidgets.QPushButton('Passed')
+        pass_button.setObjectName('pass_button')
+        pass_button.clicked.connect(self.button_clicked)
+        button_layout.addWidget(pass_button)
         fail_button = QtWidgets.QPushButton('Failed')
         fail_button.setObjectName('fail_button')
         fail_button.clicked.connect(self.button_clicked)
         button_layout.addWidget(fail_button)
 
-        pass_button = QtWidgets.QPushButton('Passed')
-        pass_button.setObjectName('pass_button')
-        pass_button.clicked.connect(self.button_clicked)
-        button_layout.addWidget(pass_button)
+        pass_button.setAutoDefault(False)
+        pass_button.setDefault(False)
+        fail_button.setAutoDefault(False)
+        fail_button.setDefault(False)
+
+
+
+        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Return), self, pass_button.click)
+        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Space), self, fail_button.click)
+        #  QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Space), self, self.button_clicked)
+
 
         self.message.connect(self.parent().xx)
 
         vertical_layout.addLayout(button_layout)
+        self.showMaximized()
         self.show()
 
     def button_clicked(self):
@@ -139,6 +119,10 @@ class MyDialog(QtWidgets.QDialog):
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MyWindow, self).__init__(*args, *kwargs)
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.CustomizeWindowHint)
+        #  self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, False)
+        #  self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, False)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setGeometry(400,400,400,400)
         self.setWindowTitle('MyWizard Example')
         centralwidget = QtWidgets.QWidget(self)
@@ -148,7 +132,11 @@ class MyWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(centralwidget)
         self.show()
         btn.clicked.connect(self.wizardshow)
+        self.showMaximized()
         self.show()
+
+    def mouseMoveEvent(self, event):
+        print('.')
 
     def wizardshow(self):
         print('aaa')
