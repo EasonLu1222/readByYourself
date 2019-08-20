23b668b5a420380004fdb07f000d50ff7a3bac61
jasonke <u912538@gmail.com>
2019-08-20 10:32:05 +0800

    Merge led_result_marker_dialog.py to task_led.py

--- tasks/task_led.py
+++ tasks/task_led.py
@@ -1,11 +1,16 @@
 import sys
+import json
 import argparse
-from PyQt5.QtWidgets import QApplication, QDialog, QLabel
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QApplication, QDialog, QLabel
 from ui.led_color_dialog import Ui_LedColorDialog
+from ui.led_result_marker_dialog import Ui_LedResultMarkerDialog
 from serials import issue_command, get_serial
-from view.led_result_marker_dialog import LedResultMarkerDialog
+
+num_key_list = [
+    Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5, Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9
+]
 
 
 class LedColorDialog(QDialog, Ui_LedColorDialog):
@@ -81,6 +86,80 @@ class LedColorDialog(QDialog, Ui_LedColorDialog):
                     cmd = f'echo {rgb[j]} > /sys/class/leds/LED{i}_{k}/brightness'
                     issue_command(ser, cmd)
 
+
+class LedResultMarkerDialog(QDialog, Ui_LedResultMarkerDialog):
+    def __init__(self, parent=None, dut_num=1):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setWindowFlags(self.windowFlags() | Qt.CustomizeWindowHint)
+        self.setWindowFlag(Qt.WindowCloseButtonHint, False)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        # self.setWindowModality(Qt.ApplicationModal)
+
+        self.color_pass = QColor("#8BC34A")  # Green
+        self.color_fail = QColor("#FF5722")  # Red
+        self.dut_num = dut_num  # Number of devices to test
+        self.result_block_list = []
+        self.pass_list = []  # Stores a list of True/False to represent pass/fail of each DUT
+        self.result_str = ''   # Json dump of pass/fail list. e.g. "['Pass', 'Fail']"
+
+        for i in range(dut_num):
+            self.add_result_block(i)
+
+        self.set_color()
+
+    def keyPressEvent(self, event):
+        # If number key is pressed
+        if event.key() in num_key_list[:self.dut_num]:
+            idx = num_key_list.index(event.key())
+            self.toggle_pass_fail(self.result_block_list[idx])
+        # If return key is pressed
+        elif event.key() == Qt.Key_Return:
+            self.close()
+        # Ignore Esc key
+        elif event.key() == Qt.Key_Escape:
+            return
+        else:
+            super(LedResultMarkerDialog, self).keyPressEvent(event)
+
+    def closeEvent(self, event):
+        pass_fail_str_list = []
+        for b in self.pass_list:
+            if b:
+                pass_fail_str_list.append('Passed')
+            else:
+                pass_fail_str_list.append('Failed')
+        self.result_str = json.dumps(pass_fail_str_list)
+        sys.stdout.write(self.result_str)
+
+    def add_result_block(self, dut_idx):
+        lb = QLabel()
+        lb.setText(str(dut_idx))
+        self.result_block_list.append(lb)
+        self.horizontalLayout.addWidget(lb)
+        self.pass_list.append(True)
+
+    def set_color(self):
+        """
+        Set the color of all result blocks
+        """
+        for lb in self.result_block_list:
+            lb.setStyleSheet(f"background-color:{self.color_pass.name()}")
+
+    def toggle_pass_fail(self, result_block):
+        """
+        Toggles the color of the result block based on the True/False value in pass_list
+
+        Args:
+            result_block: A QLabel indicating pass or fail by its background color
+        """
+        i = self.result_block_list.index(result_block)
+        self.pass_list[i] = not self.pass_list[i]
+
+        color = self.color_pass.name() if self.pass_list[i] else self.color_fail.name()
+        result_block.setStyleSheet(f"background-color:{color}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-pp', '--portnames', help='serial com port names', type=str)
@@ -90,10 +169,10 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    ser_list = []
+    serial_list = []
     for com in com_list:
         s = get_serial(com, 115200, 0)
-        ser_list.append(s)
-    d = LedColorDialog(ser_list=ser_list, dut_num=len(com_list))
+        serial_list.append(s)
+    d = LedColorDialog(ser_list=serial_list, dut_num=len(com_list))
     d.showMaximized()
     sys.exit(app.exec_())
