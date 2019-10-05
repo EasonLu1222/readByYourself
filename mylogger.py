e5c34c2d7829be815f67beaa23a69f8a429d9614
jasonke <u912538@gmail.com>
2019-10-05 17:50:38 +0800

    Add log rotate functionality
    Do log rotate every hour, keep 48 zipped log files(2 days)

--- mylogger.py
+++ mylogger.py
@@ -1,5 +1,39 @@
 import os
 import logging
+from logging.handlers import TimedRotatingFileHandler
+import zipfile
+import codecs
+import time
+import glob
+
+
+class TimedCompressedRotatingFileHandler(TimedRotatingFileHandler):
+    def doRollover(self):
+        self.stream.close()
+        # get the time that this sequence started at and make it a TimeTuple
+        t = self.rolloverAt - self.interval
+        timeTuple = time.localtime(t)
+        dfn = self.baseFilename + "." + time.strftime(self.suffix, timeTuple)
+        if os.path.exists(dfn):
+            os.remove(dfn)
+        os.rename(self.baseFilename, dfn)
+        if self.backupCount > 0:
+            # find the oldest log file and delete it
+            s = glob.glob(self.baseFilename + ".20*")
+            if len(s) > self.backupCount:
+                s.sort()
+                os.remove(s[0])
+        if self.encoding:
+            self.stream = codecs.open(self.baseFilename, 'w', self.encoding)
+        else:
+            self.stream = open(self.baseFilename, 'w')
+        self.rolloverAt = self.rolloverAt + self.interval
+        if os.path.exists(dfn + ".zip"):
+            os.remove(dfn + ".zip")
+        file = zipfile.ZipFile(dfn + ".zip", "w")
+        file.write(dfn, os.path.basename(dfn), zipfile.ZIP_DEFLATED)
+        file.close()
+        os.remove(dfn)
 
 
 def getlogger():
@@ -12,18 +46,16 @@ def getlogger():
     
     # create console handler and set level to debug
     ch1 = logging.StreamHandler()
-    ch2 = logging.FileHandler(r'logs/error_log.txt')
+    ch2 = TimedCompressedRotatingFileHandler(f"{log_dir}/log.txt", when="H", interval=1, backupCount=48)
     ch1.setLevel(logging.DEBUG)
     ch2.setLevel(logging.DEBUG)
-
     # create formatter
     formatter = logging.Formatter(
-        '[%(asctime)s]'
+        fmt='[%(asctime)s]'
         '[%(levelname)s]'
-        '[pid=%(process)d]'
-        '[thd=%(threadName)s]'
-        '[%(module)s]'
-        '%(message)s'
+        '[%(module)s#%(lineno)d]'
+        '%(message)s',
+        datefmt='%m-%d %H:%M:%S'
     )
 
     # add formatter to ch
