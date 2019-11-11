62e57a3ba7afe8f50f2cfda54fdae3829b8e00e1
zealzel <zealzel@gmail.com>
2019-11-11 21:23:51 +0700

    Change print to logger for db/sqlite.py

--- db/sqlite.py
+++ db/sqlite.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
-
 import sqlite3
 from sqlite3 import IntegrityError
+from mylogger import logger
+
 
 DB_PATH = 'C:\\db\\address.db'
 
 
 def create_table():
     conn = sqlite3.connect(DB_PATH)
-    print("Opened database successfully")
+    logger.debug(f"{PADDING}Opened database successfully")
 
     c = conn.cursor()
     c.execute('''CREATE TABLE ADDRESS
@@ -16,14 +17,14 @@ def create_table():
             ADDRESS_BT     CHAR(17)  NOT NULL UNIQUE,
             SN             CHAR(28)  UNIQUE);''')
     conn.commit()
-    print("Table created successfully")
+    logger.debug(f"{PADDING}Table created successfully")
 
     c.execute('''CREATE INDEX `` ON `ADDRESS` (
     	`ADDRESS_WIFI`	ASC,
     	`ADDRESS_BT`	ASC,
     	`SN`	ASC);''')
     conn.commit()
-    print("Index created successfully")
+    logger.debug(f"{PADDING}Index created successfully")
     conn.close()
 
 
@@ -40,7 +41,7 @@ def gen_mac_list(start_mac, total_mac):
 def import_addr(addr_list):
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    print("Opened database successfully")
+    logger.debug(f"{PADDING}Opened database successfully")
 
     for i in range(0, len(addr_list)):
         if (i % 2) == 0:
@@ -48,21 +49,21 @@ def import_addr(addr_list):
                 c.execute(f'INSERT INTO ADDRESS (ADDRESS_WIFI,ADDRESS_BT,SN) \
                     VALUES ("{addr_list[i]}", "{addr_list[i + 1]}", null )')
             except IntegrityError as e:
-                print(f"Error inserting ({addr_list[i]}, {addr_list[i + 1]}), {e}")
+                logger.error(f"Error inserting ({addr_list[i]}, {addr_list[i + 1]}), {e}")
 
     conn.commit()
-    print("Records created successfully")
+    logger.debug(f"{PADDING}Records created successfully")
     conn.close()
 
 
 def write_addr(addr, sn):
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    print("Opened database successfully")
+    logger.debug(f"{PADDING}Opened database successfully")
 
     c.execute(f'UPDATE ADDRESS set SN = "{sn}" where ADDRESS_WIFI="{addr}"')
     conn.commit()
-    print("Total number of rows updated :", conn.total_changes)
+    logger.debug(f"{PADDING}Total number of rows updated :", conn.total_changes)
     conn.close()
 
 
@@ -71,15 +72,15 @@ def fetch_addr():
     mac_bt = ""
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    print("Opened database successfully")
+    logger.debug(f"{PADDING}Opened database successfully")
 
     cursor = c.execute("SELECT ADDRESS_WIFI, ADDRESS_BT from ADDRESS where SN is null")
     result = cursor.fetchone()
     try:
         mac_wifi, mac_bt = result
-        print("Operation  successfully")
+        logger.debug(f"{PADDING}Operation  successfully")
     except TypeError as e:
-        print("Error: no WiFi or BT address available")
+        logger.error(f"{PADDING}Error: no WiFi or BT address available")
     conn.close()
     return f"{mac_wifi},{mac_bt}"
 
@@ -87,11 +88,11 @@ def fetch_addr():
 def is_addr_used(addr):
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    print("Opened database successfully")
+    logger.debug(f"{PADDING}Opened database successfully")
 
     cursor = c.execute(f"SELECT SN from ADDRESS where ADDRESS_WIFI={addr}")
 
-    print("Operation done successfully")
+    logger.debug(f"{PADDING}Operation done successfully")
     conn.close()
 
 
