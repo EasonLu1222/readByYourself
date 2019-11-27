8d94886ab7ce639611adef03c963a2275ebe3f8d
jasonke <u912538@gmail.com>
2019-11-27 09:50:09 +0800

    Add error handling for SFC request

--- sfc.py
+++ sfc.py
@@ -2,6 +2,7 @@ import re
 import requests
 import pandas as pd
 from mylogger import logger
+from requests.exceptions import ConnectTimeout
 
 PADDING = ' ' * 8
 
@@ -17,9 +18,10 @@ endpoint_dict = {
 }
 
 
-def send_result_to_sfc(d=None, sfc_station_id='SA01', msn=None, res='Pass', dut_num=1, dut_i=0, t0='2019/11/25 08:00:00', t1='2019/11/25 08:00:00'):
+def send_result_to_sfc(d, sfc_station_id, msn, res, dut_num, dut_i, t0, t1):
+# def send_result_to_sfc(d=None, sfc_station_id='SA01', msn=None, res='Pass', dut_num=1, dut_i=0, t0='2019/11/27 08:00:00', t1='2019/11/27 08:00:00'):
     # d.to_pickle('../sa_test_result.pkl')
-    d = pd.read_pickle('../sa_test_result.pkl')
+    # d = pd.read_pickle('../sa_test_result.pkl')
 
     df = d[(d.hidden == False) & (d.sfc_name != "")]
     cols1 = (df.sfc_name).values.tolist()
@@ -46,7 +48,20 @@ def send_result_to_sfc(d=None, sfc_station_id='SA01', msn=None, res='Pass', dut_
     except IndexError as ex:
         logger.error(f"{PADDING}cannot generate POST data")
     endpoint = endpoint_dict[post_data['station_id']]
-    url = f"{SFC_HOST}/{endpoint}"
+    url = f"{SFC_HOST}/{endpoint}.asp"
 
-    r = requests.post(url=endpoint, data=post_data)
-    logger.info(f"{PADDING}SFC respond: {r.text}")
+    try:
+        logger.info(f"{PADDING}Requesting {url} with data:{post_data}")
+        r = requests.post(url=url, data=post_data)
+        if r.status_code != 200:
+            logger.error(f"{PADDING}The status code is{r.status_code}")
+        else:
+            logger.info(f"{PADDING}SFC response: {r.text}")
+    except ConnectTimeout:
+        logger.error(f"{PADDING}Connection timeout")
+    except ConnectionError:
+        logger.error(f"{PADDING}Connection error")
+    except Exception as ex:
+        logger.error(f"{PADDING}SFC request error: {ex}")
+
+# send_result_to_sfc()
