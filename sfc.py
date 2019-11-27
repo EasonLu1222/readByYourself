d5dc1b41179483a104c27e85088a9cee79d83ce0
jasonke <u912538@gmail.com>
2019-11-27 09:54:03 +0800

    Use production SFC host

--- sfc.py
+++ sfc.py
@@ -6,8 +6,8 @@ from requests.exceptions import ConnectTimeout
 
 PADDING = ' ' * 8
 
-SFC_HOST = 'http://10.228.14.99:7109'   # debug
-# SFC_HOST = 'http://10.228.16.99'  # production
+# SFC_HOST = 'http://10.228.14.99:7109'   # debug
+SFC_HOST = 'http://10.228.16.99'  # production
 
 endpoint_dict = {
     'SA01': 'add_data_sa',
