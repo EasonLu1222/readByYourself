4f977b667ab387a5369551ec545620bcb1409fbf
jasonke <u912538@gmail.com>
2019-12-13 18:04:14 +0700

    Integrate version checker to app

--- tools/auto_update/version_checker.py
+++ tools/auto_update/version_checker.py
@@ -1,78 +1,99 @@
 import os
+import time
 import socket
+import sys
 from ftplib import FTP
+from PyQt5.QtWidgets import QApplication
+from mylogger import logger
+from PyQt5.QtCore import QThread, pyqtSignal
 
 LOCAL_APP_PATH = 'D:\\SAP109_STATION'
 
 
-def check_for_update():
-    """
-    Compare the local app version with remote one(on FTP)
+class VersionChecker(QThread):
 
-    Returns:
-        Bool, True if a newer version exist, False otherwise
-    """
-    ftp = get_ftp()
-    if not ftp:
-        return False
+    version_checked = pyqtSignal(object)
 
-    file_list = ftp.nlst()
-    latest_app_name = get_app_name_in_dir(file_list)
+    def __init__(self):
+        QThread.__init__(self)
 
-    if not latest_app_name:
-        print("Error: latest app not found")
-        return False
+    def run(self):
+        can_update = self.is_update_available()
+        self.version_checked.emit(can_update)
 
-    file_list = os.listdir(LOCAL_APP_PATH)
-    current_app_name = get_app_name_in_dir(file_list)
-    print(f'Remote app name: {latest_app_name}')
-    print(f'Local app name : {current_app_name}')
+    def is_update_available(self):
+        """
+        Compare the local app version with remote one(on FTP)
 
-    ftp.quit()
+        Returns:
+            Bool, True if a newer version exist, False otherwise
+        """
+        ftp = self.get_ftp()
+        if not ftp:
+            return False
 
-    is_update_available = latest_app_name>current_app_name
-    print(f"Need update: {is_update_available}")
+        file_list = ftp.nlst()
+        latest_app_name = self.get_app_name_in_dir(file_list)
 
-    return is_update_available
+        if not latest_app_name:
+            logger.error("Error: latest app not found")
+            return False
 
+        try:
+            file_list = os.listdir(LOCAL_APP_PATH)
+        except FileNotFoundError:
+            logger.error("Error: local app not found")
+            return False
+        current_app_name = self.get_app_name_in_dir(file_list)
+        logger.info(f'Remote app name: {latest_app_name}')
+        logger.info(f'Local app name : {current_app_name}')
 
-def get_ftp():
-    """
-    Connect to the FTP and navigate to the Latest_App folder
-    Returns:
-        ftp object
-    """
-    rtn = None
-    try:
-        ftp = FTP('10.228.14.92', timeout=3)
-        ftp.login(user='SAP109', passwd='sapsfc')
-        ftp.cwd('Belkin109/Latest_App')
-        rtn = ftp
-    except socket.timeout as e:
-        print('Error: FTP connection timeout')
-    except OSError as e:
-        print('Error: Network is unreachable')
+        ftp.quit()
 
-    return rtn
+        can_update = latest_app_name > current_app_name
+        logger.info(f"Need update: {can_update}")
 
+        return can_update
 
-def get_app_name_in_dir(file_list):
-    """
-    Get the app name from the file_list
-    Args:
-        file_list: An array of file name
+    def get_ftp(self):
+        """
+        Connect to the FTP and navigate to the Latest_App folder
+        Returns:
+            ftp object
+        """
+        rtn = None
+        try:
+            ftp = FTP('10.228.16.92', timeout=3)
+            ftp.login(user='SAP109', passwd='sapsfc')
+            ftp.cwd('Belkin109/Latest_App')
+            rtn = ftp
+        except socket.timeout as e:
+            logger.error('Error: FTP connection timeout')
+        except OSError as e:
+            logger.error('Error: Network is unreachable')
 
-    Returns:
-        app name, e.g.: app_20191212_1131.exe
-    """
-    latest_app_name = None
-    for file_name in file_list:
-        if file_name.startswith("app"):
-            latest_app_name = file_name
-            break
+        return rtn
 
-    return latest_app_name
+    def get_app_name_in_dir(self, file_list):
+        """
+        Get the app name from the file_list
+        Args:
+            file_list: An array of file name
+
+        Returns:
+            app name, e.g.: app_20191212_1131.exe
+        """
+        latest_app_name = None
+        for file_name in file_list:
+            if file_name.startswith("app"):
+                latest_app_name = file_name
+                break
+
+        return latest_app_name
 
 
 if __name__ == "__main__":
-    check_for_update()
+    app = QApplication(sys.argv)
+    vc = VersionChecker()
+    vc.start()
+    time.sleep(4)
