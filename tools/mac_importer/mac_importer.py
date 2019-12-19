714ecb00527675097cec8786de2b63359d0892a9
jasonke <u912538@gmail.com>
2019-12-19 11:05:51 +0700

    Add mac_importer.py, a tool for importing MAC address to DB

---
+++ tools/mac_importer/mac_importer.py
@@ -0,0 +1,94 @@
+import os
+import sqlite3
+from pathlib import Path
+from sqlite3 import IntegrityError, OperationalError
+from gooey import Gooey, GooeyParser
+
+DB_DIR = 'C:\\db'
+DB_PATH = os.path.join(DB_DIR, 'address.db')
+
+
+@Gooey(program_name="SAP109 MAC Address Importer", show_restart_button=False)
+def main():
+    desc = "MAC address importer"
+    file_help_msg = "Name of the file you want to process"
+    my_cool_parser = GooeyParser(description=desc)
+    my_cool_parser.add_argument("FileChooser", help=file_help_msg, widget="FileChooser")
+    args = my_cool_parser.parse_args()
+
+    mac_range_path = args.FileChooser
+    with open(mac_range_path, 'r') as f:
+        mac_range = f.read()
+        start_mac, range = mac_range.split(',')
+
+    mac_list = gen_mac_list(int(start_mac, 16), int(range))
+    import_addr(mac_list)
+    print('\n')
+
+
+def gen_mac_list(start_mac, total_mac):
+    mac_list = []
+    for i in range(total_mac):
+        mac = format(start_mac + i, 'x')  # Convert hex number to hex string, e.g. 0xc4411e6a09ac to "c4411e6a09ac"
+        ml = [mac[i:i + 2] for i in range(0, len(mac), 2)]
+        mac_str = ":".join(ml)
+        mac_list.append(mac_str)
+    return mac_list
+
+
+def import_addr(addr_list):
+    total_inserted = 0
+    total_duplicated = 0
+
+    if not os.path.exists(DB_DIR):
+        os.makedirs(DB_DIR)
+        print("DB folder not exist. Creating DB folder")
+    else:
+        print("DB folder found")
+
+    if Path(DB_PATH).is_file():
+        print("DB found")
+    else:
+        create_table()
+
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    print(f"Opened database successfully")
+
+    for i in range(0, len(addr_list)):
+        if (i % 2) == 0:
+            try:
+                c.execute(f'INSERT INTO ADDRESS (ADDRESS_WIFI,ADDRESS_BT,SN) \
+					VALUES ("{addr_list[i]}", "{addr_list[i + 1]}", null )')
+                total_inserted += 1
+            except IntegrityError as e:
+                total_duplicated += 1
+    conn.commit()
+    conn.close()
+    print("-----------------------------------")
+    print(f"Inserted: {total_inserted}\nDuplicated: {total_duplicated}\nTotal: {total_inserted + total_duplicated}")
+
+
+def create_table():
+    conn = sqlite3.connect(DB_PATH)
+    print(f"Opened database successfully")
+
+    c = conn.cursor()
+    c.execute('''CREATE TABLE ADDRESS
+        (ADDRESS_WIFI   CHAR(17)  NOT NULL UNIQUE,
+        ADDRESS_BT     CHAR(17)  NOT NULL UNIQUE,
+        SN             CHAR(28)  UNIQUE);''')
+    conn.commit()
+    print(f"Table created successfully")
+
+    c.execute('''CREATE INDEX `` ON `ADDRESS` (
+        `ADDRESS_WIFI`	ASC,
+        `ADDRESS_BT`	ASC,
+        `SN`	ASC);''')
+    conn.commit()
+    print(f"Index created successfully")
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
