EasonLu1222/readByYourself

commit 5063d9e6c690d77781db2dc5ebf8f416a59122e1

Move CSV file to mb_log folder when all data are ready

app.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import json
 import time
@@ -792,6 +793,11 @@ class MyWindow(QMainWindow, Ui_MainWindow):
 
                 with open(self.logfile, 'a') as f:
                     dd.to_csv(f, mode='a', header=f.tell()==0, sep=',', line_terminator='\n')
+            try:
+                shutil.move(f'./logs/{csv_filename}', f'./logs/mb_log')
+            except FileNotFoundError as e:
+                logger.debug(f"{e}")
+
 
             self.set_window_color('pass' if all(e == 'Pass' for e in all_res) else 'fail')
             self.table_view.setFocusPolicy(Qt.NoFocus)

sfc.py
@@ -103,10 +103,10 @@ def gen_ks_sfc_csv(d, csv_filename, station, msn, part_num, dut_num, dut_i, resu
     dd = dd.assign(**cols2_value)[list(cols2_value) + cols1]
     dd = dd.assign(**{'result': result})
 
-    if not os.path.exists('./logs/mb_log'):
-        os.makedirs('./logs/mb_log')
+    if not os.path.exists('./logs'):
+        os.makedirs('./logs')
 
-    with open(f'./logs/mb_log/{csv_filename}', 'a') as f:
+    with open(f'./logs/{csv_filename}', 'a') as f:
         dd.to_csv(f, index=False, mode='a', header=f.tell() == 0, sep=',', line_terminator='\n')
 
 

commit 78effeb9b42147aa72dfe4d0427037ef2d08e584

Fix CSV path bug introduced by commit 5063d9e

app.py
@@ -794,7 +794,7 @@ class MyWindow(QMainWindow, Ui_MainWindow):
                 with open(self.logfile, 'a') as f:
                     dd.to_csv(f, mode='a', header=f.tell()==0, sep=',', line_terminator='\n')
             try:
-                shutil.move(f'./logs/{csv_filename}', f'./logs/mb_log')
+                shutil.move(f'./logs/{csv_filename}', f'./logs/mb_log/{csv_filename}')
             except FileNotFoundError as e:
                 logger.debug(f"{e}")
 

sfc.py
@@ -103,8 +103,7 @@ def gen_ks_sfc_csv(d, csv_filename, station, msn, part_num, dut_num, dut_i, resu
     dd = dd.assign(**cols2_value)[list(cols2_value) + cols1]
     dd = dd.assign(**{'result': result})
 
-    if not os.path.exists('./logs'):
-        os.makedirs('./logs')
+    os.makedirs('./logs/mb_log', exist_ok=True)
 
     with open(f'./logs/{csv_filename}', 'a') as f:
         dd.to_csv(f, index=False, mode='a', header=f.tell() == 0, sep=',', line_terminator='\n')